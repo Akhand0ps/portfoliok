584373b67b57ef59dad9a61dc7a3920f2f27687f
Akhand0ps <akhandps08@gmail.com>
2025-05-31 18:28:45 +0530

    initial commit: my portfolioo

---
+++ README.md
@@ -0,0 +1,4 @@
+# Akhand Pratap Singh - Portfolio
+
+This repository contains the source code for my personal developer portfolio website. It showcases my projects, skills, education, and achievements.  
+You can view the live si te or use this code as a reference for building your own portfolio.
---
+++ src/constants/index.js
@@ -0,0 +1,213 @@
+import {
+  frontend,
+  backend,
+  ux,
+  prototyping,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  git,
+  figma,
+  docker,
+  postgresql,
+  rubyrails,
+  graphql,
+  komikult,
+  leaderboard,
+  math,
+  movie,
+  nyeusi,
+  space,
+  coverhunt,
+  dcc,
+  kelhel,
+  microverse,
+} from '../assets';
+
+export const navLinks = [
+  {
+    id: 'about',
+    title: 'About',
+  },
+  {
+    id: 'projects',
+    title: 'Projects',
+  },
+  {
+    id: 'contact',
+    title: 'Contact',
+  },
+];
+
+const services = [
+  {
+    title: 'Frontend Developer',
+    icon: frontend,
+  },
+  {
+    title: 'Backend Developer',
+    icon: backend,
+  },
+  
+  {
+    title: 'Software Prototyping',
+    icon: prototyping,
+  },
+];
+
+const technologies = [
+  {
+    name: 'HTML 5',
+    icon: html,
+  },
+  {
+    name: 'CSS 3',
+    icon: css,
+  },
+  {
+    name: 'JavaScript',
+    icon: javascript,
+  },
+  {
+    name: 'TypeScript',
+    icon: typescript,
+  },
+  {
+    name: 'React JS',
+    icon: reactjs,
+  },
+  {
+    name: 'Redux Toolkit',
+    icon: redux,
+  },
+  {
+    name: 'Tailwind CSS',
+    icon: tailwind,
+  },
+  {
+    name: 'Node JS',
+    icon: nodejs,
+  },
+  
+  
+  {
+    name: 'postgresql',
+    icon: postgresql,
+  },
+  {
+    name: 'git',
+    icon: git,
+  },
+  {
+    name: 'figma',
+    icon: figma,
+  },
+  {
+    name: 'docker',
+    icon: docker,
+  },
+];
+
+const experiences = [
+  
+];
+
+const projects = [
+  {
+    id: 'project-1',
+    name: 'DeadLockHub',
+    description: 'A web app to visualize deadlocak state and find safe paths.',
+    tags: [
+      {
+        name: 'react',
+        color: 'blue-text-gradient',
+      },
+      {
+        name: 'mongodb',
+        color: 'green-text-gradient',
+      },
+      {
+        name: 'tailwind',
+        color: 'pink-text-gradient',
+      },
+    ],
+    image: komikult,
+    repo: 'https://github.com/Akhand0ps/BackendForDeadlock',
+    demo: 'https://deadlock-mu.vercel.app/index.html',
+  },
+  {
+    id: 'project-2',
+    name: 'TaskDeck',
+    description:
+      'Mini web app For managing tasks and can be set to public/private.',
+    tags: [
+      {
+        name: 'react',
+        color: 'blue-text-gradient',
+      },
+      {
+        name: 'restapi',
+        color: 'green-text-gradient',
+      },
+      {
+        name: 'scss',
+        color: 'pink-text-gradient',
+      },
+    ],
+    image: leaderboard,
+    repo: 'https://github.com/Akhand0ps/taskdeck',
+    demo: 'https://deadlock-mu.vercel.app/index.html',
+  },
+  {
+    id: 'project-3',
+    name: 'MusicStream',
+    description: 'Music streaming web app with chatbot that allows users to search for songs and artists .',
+    tags: [
+      {
+        name: 'nextjs',
+        color: 'blue-text-gradient',
+      },
+      {
+        name: 'supabase',
+        color: 'green-text-gradient',
+      },
+      {
+        name: 'css',
+        color: 'pink-text-gradient',
+      },
+    ],
+    image: math,
+    repo: 'https://github.com/Akhand0ps/MusicStream',
+    demo: '',
+  },
+  {
+    id: 'project-4',
+    name: 'Course Backend',
+    description: `A backend for a course management system that allows users to create, update, and delete courses with transactions.`,
+    tags: [
+      {
+        name: 'nextjs',
+        color: 'blue-text-gradient',
+      },
+      {
+        name: 'supabase',
+        color: 'green-text-gradient',
+      },
+      {
+        name: 'css',
+        color: 'pink-text-gradient',
+      },
+    ],
+    image: movie,
+    repo: 'https://github.com/Akhand0ps/course-backend',
+    demo: '',
+  },
+  
+];
+
+export { services, technologies, experiences, projects };
